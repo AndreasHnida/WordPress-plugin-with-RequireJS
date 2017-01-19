14c1c387afa7c5e5e4b160fbef12ac30057cdb4a
Andreas Hnida <Andreas.Hnida@Gmail.com>
2017-01-19 19:47:53 +0100

    another try

--- js/app.build.js
+++ js/app.build.js
@@ -3,8 +3,8 @@
   // TO RUN THE BUILD, GET TO THIS DIR IN THE COMMAND LINE AND RUN: node r.js -o app.build.js
 
   // The path of the dev files (main shouild be directly underneath this)
-  baseUrl: '.',
-    dir: '.',
+  baseUrl: 'js',
+    dir: 'js/lib',
   // Output everything to one minified file
 
   // Comment out the below line to minify using UglifyJS
