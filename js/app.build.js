e69ffb47a0148f36dc6308c643380bdc76c4506d
Andreas Hnida <Andreas.Hnida@Gmail.com>
2017-01-20 13:38:25 +0100

    new foo.js

--- js/app.build.js
+++ js/app.build.js
@@ -3,8 +3,8 @@
   // TO RUN THE BUILD, GET TO THIS DIR IN THE COMMAND LINE AND RUN: node r.js -o app.build.js
 
   // The path of the dev files (main shouild be directly underneath this)
-  baseUrl: 'js',
-    dir: 'foo',
+  baseUrl: '../js',
+    dir: 'js',
   // Output everything to one minified file
 
   // Comment out the below line to minify using UglifyJS
---
+++ js/js/lib/jquery.js
@@ -0,0 +1,1 @@
+foo.define(["lib/jquery-1.9.0.min"],function(){return jQuery.noConflict(!0)});
